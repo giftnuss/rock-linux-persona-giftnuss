6daba0e2194980eab1365172cee25256798fcc17
root <root@neon.(none)>
2009-06-06 21:27:55 +0200

    imported git-send improvements from sm

--- misc/archive/git-send.sh
+++ misc/archive/git-send.sh
@@ -4,6 +4,9 @@ URL=https://www.rocklinux.net/submaster/
 
 if [ $# -lt 3 ]; then
 	echo
+	echo "Usage: $0 { username password | - } file(s)"
+	echo
+	echo
 	echo "Usage example:"
 	echo
 	echo "   git-format-patch HEAD~4"
@@ -13,10 +16,17 @@ if [ $# -lt 3 ]; then
 fi
 
 username="$1"; shift
-password="$1"; shift
+if [ "$username" != "-" ]; then
+	password="$1"; shift
+fi
 
+i=0
 for p; do
-	awk 'pass { print; next; } /^From:/ { gsub(/^From: /, ""); gsub(/<.*/, ""); name=$0; next; } /^Subject:/ { gsub(/^.*PATCH\] */, ""); message="\t" $0; next; } /^---/ { print "\n" name ":\n" message "\n"; next; } /^diff/ { print; pass=1; next; } /./ { message = message "\n\t" $0; }' < "$p" > git-send-tmp.txt
-	curl -k -F u="$username" -F p="$password" -F a=new -F q=1 -F f=\@git-send-tmp.txt "$URL/smadm.cgi"
+	fn=`printf "git-send-tmp%03d.txt" $i`
+	awk 'pass { print; next; } /^From:/ { gsub(/^From: /, ""); gsub(/ *<.*/, ""); name=$0; next; } /^Subject:/ { gsub(/^.*PATCH\] */, ""); message="\t" $0; next; } /^---/ { print "\n" name ":\n" message "\n"; next; } /^diff/ { print; pass=1; next; } /./ { message = message "\n\t" $0; }' < "$p" > $fn
+	if [ "$username" != "-" ]; then
+		curl -k -F u="$username" -F p="$password" -F a=new -F q=1 -F f=\@$fn "$URL/smadm.cgi"
+	fi
+	i=`expr $i + 1`
 done
 
