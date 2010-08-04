f2e839c88722b1629009624c25cbafaac8267366
root <root@tribe.(none)>
2010-08-04 18:19:16 +0000

    stf: fix libxml2 for cross builds

--- package/base/libxml2/update_catalog.sh
+++ package/base/libxml2/update_catalog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 . /etc/profile
-rm -f /etc/xml/catalog
-xmlcatalog --noout --create /etc/xml/catalog
+rm -f $root/etc/xml/catalog
+xmlcatalog --noout --create $root/etc/xml/catalog
 for catfile in `echo $XML_CATALOG_FILES | tr ' ' '\n' | sort -u`; do
-	xmlcatalog --noout --add nextCatalog $catfile "" /etc/xml/catalog
+	xmlcatalog --noout --add nextCatalog $catfile "" $root/etc/xml/catalog
 done
 
