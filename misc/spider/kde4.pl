358abeb063863405b2a977087ba42ff78bc5daf0
Sebastian B. Knapp <rock@ccls-online.de>
2012-03-27 23:58:21 +0200

    welcome back spider

---
+++ misc/spider/kde4.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+
+use 5.010;
+
+use FindBin;
+use lib $FindBin::Bin . '/lib';
+
+use ROCK::Linux;
+
+use IO::Dir;
+use IO::File;
+use Mojo::UserAgent;
+
+my $version = $ARGV[0] || 'list';
+
+my $hive = './package/kde4';
+
+if($version eq 'list') {
+    my $d = IO::Dir->new($hive) or die($!);
+    while (defined(my $p = $d->read)) {
+       my $fh = IO::File->new("$hive/$p/$p.desc","r") or next;
+       my ($pkgversion) = map { chomp; s/^\[V\] //; $_ } 
+           grep { /\[V\]/ } <$fh>;
+       say "$p $pkgversion";
+    }
+}
+
+my $url = 'http://www.kde.org/info/' . $version . '.php';
+
+my $client = Mojo::UserAgent->new;
+
+my $res = $client->get($url)->res;
+
+if($res->is_status_class(200)) {
+    my $dom = $res->dom;
+    for my $e ($dom->find("td a")->each) {
+        my $text = $e->text;
+        $text =~ /^(.*)-(.*)$/ or next;
+        my ($name,$version) = ($1,$2);
+        my $dwnl = $e->attrs->{'href'};
+        $dwnl =~ /(.*\/)(.*)$/;
+        my ($path,$file) = ($1,$2);
+        
+        my $pkg = "${name}4";
+        apply_pkg_update($pkg,$version);
+        pkg_change_download($pkg,$file,$path);
+    }
+}
+#->dom->at('title')->text;
+
---
+++ misc/spider/lib/ROCK/Linux.pm
@@ -0,0 +1,129 @@
+package ROCK::Linux;
+# Abstract: something that works with ROCK Linux
+
+use strict;
+use warnings;
+use utf8;
+use 5.010;
+use Cwd;
+use IO qw/Dir File Pipe/;
+
+use base 'Exporter';
+
+our @EXPORT = qw/
+    pkg_refresh
+    pkg_change_download
+    apply_copypatch
+    apply_pkg_update
+    apply_cksumpatch
+    download
+/;
+our ($DIR,$SCRIPTS);
+
+BEGIN {
+   $DIR ||= $ENV{'ROCK_BASE'} || Cwd::getcwd; # adjust
+   $SCRIPTS = "$DIR/scripts";
+
+   IO::Dir->new($SCRIPTS) or die("Error reading scripts dir: $!");
+   my $patch = `which patch`;
+   chomp $patch;
+   sub PATCH { "$patch -p0" }
+};
+
+sub CREATE_PKGUPDPATCH { "$SCRIPTS/Create-PkgUpdPatch" }
+sub CREATE_CKSUMPATCH { "$SCRIPTS/Create-CkSumPatch" }
+sub CREATE_COPYPATCH { "$SCRIPTS/Create-CopyPatch" }
+sub DOWNLOAD { "$SCRIPTS/Download" }
+
+sub pkg_refresh {
+    my ($pkg,$version) = @_;
+    apply_pkg_update($pkg,$version);
+    apply_copypatch($pkg);
+    download($pkg);
+    apply_cksumpatch($pkg);
+}
+
+sub apply_pkg_update {
+    my ($pkg,$version) = @_;
+    my $cmd = CREATE_PKGUPDPATCH . " $pkg~$version";
+    _apply_cmd($cmd);
+}
+
+sub apply_copypatch {
+    my ($pkg) = @_;
+    my $cmd = CREATE_COPYPATCH . " package/*/$pkg";
+    _apply_cmd($cmd);
+}
+
+sub apply_cksumpatch {
+    my ($pkg) = @_;
+    my $cmd = CREATE_CKSUMPATCH . " $pkg";
+    _apply_cmd($cmd);
+}
+
+sub _apply_cmd {
+    my ($cmd) = @_;
+    my ($read,$write) = map  { IO::Pipe->new } 0..1;
+    $read->reader($cmd);
+    $write->writer(PATCH);
+    while(<$read>) {
+        print $write $_;
+    }
+}
+
+sub download {
+    my ($pkg) = @_;
+    system(DOWNLOAD . " $pkg");
+}
+
+sub desc_file {
+    my ($pkg) = @_;
+    my $path = `ls package/*/$pkg/$pkg.desc`;
+    if($? != 0) {
+        die("Package $pkg not found!");
+    }
+    chomp $path;
+    return $path;
+}
+
+sub pkg_change_download {
+    my ($pkg,$file,$path) = @_;
+    my $desc = desc_file($pkg);
+    my $read = IO::File->new($desc,'r') or die($!);
+    my @lines = <$read>;
+    $read->close;
+    my $change;
+    for my $i (0..$#lines) {
+        my $line = $lines[$i];
+        $line =~ /^(\[D\])\s+(\S+)\s+(\S+)\s(.+)$/ or next;
+        my ($pre,$ck,$fn,$pn) = ($1,$2,$3,$4);
+        last if $file eq $fn && $path eq $pn;
+        die("multiple downloads not supported")
+            if defined($change);
+        $change = 0;
+        my @n = ($pre);
+        if($file eq $fn || !defined($file)) {
+            push @n, $ck, $fn;
+        }
+        else {
+            push @n, '0', $file;
+            $change++;
+        }
+        if($path eq $pn || !defined($path)) {
+            push @n, $pn, "\n";
+        }
+        else {
+            push @n, $path, "\n";
+            $change++;
+        }
+        return unless $change;
+        $lines[$i] = join " ", @n;
+    }
+    my $write = IO::File->new($desc,"w") or die($!);
+    $write->print($_) for @lines;
+}
+
+1;
+
+__END__
+
