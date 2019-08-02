ba64a1cb02f0762ac39ddb102f4a408fc3690592
Sebastian B. Knapp <news@young-workers.de>
2019-08-02 17:20:57 +0200

    modularize and test II

--- tests/test-build-functions.sh
+++ tests/test-build-functions.sh
@@ -8,7 +8,7 @@ source scripts/build-functions
 
 # helpers
 set_builddir () {
-    builddir=$(mktemp -d -p .)
+    builddir=$(mktemp -d -p $(pwd))
 }
 clean_builddir () {
     rm -rf $builddir
@@ -17,7 +17,7 @@ clean_builddir () {
 ### -------------------------
 
 exec 202>&1
-base=.
+base=$(pwd)
 download=0
 desc_D="467800010 dash-0.5.10.tar.gz http://gondor.apana.org.au/~herbert/dash/files/"
 
@@ -40,6 +40,35 @@ build_set_sourceballs
 assert "echo $xsourceballs" dash-0.5.10.tar.bz2
 assert_end build_set_sourceballs
 
+### -------------------------
 
+exec 202>&1
+base=$(pwd)
+download=0
+desc_D="467800010 dash-0.5.10.tar.gz http://gondor.apana.org.au/~herbert/dash/files/"
+taropt="--use-compress-program=bzip2 -xf"
+xsrctar=dash-0.5.10.tar.bz2
+set_builddir
+cd $builddir
+build_setup_archdir >/dev/null
+
+assert_raises '[ -d $archdir ]'
+assert_raises '[ -L $archdir/$xsrctar ]'
+build_autoextract_source
+assert_raises '[ -s untar.txt ]'
+assert_raises '[ -s xsrcdir.txt ]'
+assert_raises '[ -d $builddir/dash-0.5.10 ]'
 
 assert_end build_autoextract_source
+
+srcdir=auto
+
+build_set_xsrcdir
+assert 'echo $xsrcdir' dash-0.5.10
+
+assert_end build_set_xsrcdir
+cd - >/dev/null
+clean_builddir
+
+### -------------------------
+
